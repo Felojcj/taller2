f2f9bfc5613a60e32cf39ad21f524c500b42ef76
Felojcj <felocorrea18@gmail.com>
2022-02-14 20:51:44 -0500

    Prizes column added

--- src/views/Questions/Questions.tsx
+++ src/views/Questions/Questions.tsx
@@ -5,11 +5,15 @@ import Typography from "@mui/material/Typography";
 
 const Questions = () => {
   const { state } = useLocation();
-  const [userData, setUserData] = useState<{user: string, difficulty:string}>();
+  const [userData, setUserData] =
+    useState<{ user: string; difficulty: string }>();
+  const prizes = [10000, 9000, 8000, 7000, 6000, 5000, 4000, 3000, 2000, 1000];
+  const options2 = { currency: "USD" };
+  const numberFormat2 = new Intl.NumberFormat("en-US", options2);
 
   useEffect(() => {
     const data: any = state;
-    setUserData(data)
+    setUserData(data);
     if (data.difficulty !== "Any Difficulty") {
       axios
         .get(
@@ -28,8 +32,8 @@ const Questions = () => {
   }, []);
 
   return (
-    <div>
-      <div className="d-flex justify-content-between border border-dark p-2">
+    <div className="vh-100">
+      <div className="d-flex justify-content-between border-bottom border-dark p-2" style={{ height: "8%" }}>
         <Typography variant="h5" component="div">
           Player Name: {userData?.user}
         </Typography>
@@ -39,6 +43,23 @@ const Questions = () => {
         <Typography variant="h5" component="div">
           Earnings: 0$
         </Typography>
+      </div>
+      <div className="row m-0" style={{ height: "92%" }}>
+        <div className="col-9">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            architecto quae doloribus fugiat quas praesentium, repellat facere
+            veniam neque asperiores laboriosam id consectetur velit numquam
+            cumque iste a, deleniti magnam!
+          </p>
+        </div>
+        <div className="col-3 p-5 border-start border-dark">
+          {prizes.map((prize, index, prizes) => (
+            <Typography variant="h5" component="div">
+              {`${prizes.length - index} $${numberFormat2.format(prize)}`}
+            </Typography>
+          ))}
+        </div>
       </div>
     </div>
   );
